b85c28108e04ce74d20c1cd975b45d37eafa74be
Дмитрий С <112376316+DSvistelnik@users.noreply.github.com>
2022-12-29 10:11:12 +0300

    2/0

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, abort
+from container import checker, user_request
 
 app = Flask(__name__)
 
 
-@app.route("/perform_query", methods=['GET'])
+@app.route("/perform_query/", methods=['GET'])
 def perform_query():
     # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
     # проверить, что файла file_name существует в папке DATA_DIR, при ошибке вернуть ошибку 400
@@ -13,4 +14,17 @@ def perform_query():
 
     if not checker.is_valid(args):
         abort(400)
-    return app.response_class('', content_type="text/plain")
+
+    try:
+        result = user_request.execute(**args)
+
+    except Exception as e:
+        abort(400)
+
+    if not result:
+        abort(404)
+
+    return app.response_class(result, content_type="text/plain")
+
+if __name__ == '__main__':
+    app.run()
--- container.py
+++ container.py
@@ -1,10 +1,13 @@
 from DAO import FileDao
 from service import FileService
 from checking_arguments import ArgumentsChecker
+from request import Request
+from constans import QUERY
 
 
 dao = FileDao()
 service = FileService(dao)
 checker = ArgumentsChecker()
+user_request = Request(service, QUERY)
 
 
